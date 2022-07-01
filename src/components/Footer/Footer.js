0ad3c92d13d65c943151b44fedb359cbfe3ef5e4
JimmyZ98 <sam.jimmy.zhen@gmail.com>
2022-07-01 19:20:29 -0400

    created and styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,19 @@
 import React from "react";
+import logo from "../../assets/Logo/YGOMarket-logo.png";
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <p className="footer__text">
+        © 2022 — Designed and Developed by Jimmy Zhen
+      </p>
+      <Link to="/" className="footer__logo-link">
+        <img className="footer__logo" src={logo} alt="ygomarket logo" />
+      </Link>
+    </div>
+  );
 }
 
 export default Footer;
